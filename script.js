9c9fde47c0c7597502ab2e6f3f76c1ac103f57f7
Melissa8880 <email>
2025-02-26 12:44:37 +1100

    Add student score calculator and launch configuration for debugging

---
+++ script.js
@@ -0,0 +1,33 @@
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+
+console.log("Student Score Calculator");
+console.log("========================");
+
+let score = 55;
+
+if (score < 60) {
+    console.log("F");
+}
+else if (score < 70) {
+    console.log("D");
+}
+else if (score < 80) {
+    console.log("C");
+}
+else if (score < 90) {
+    console.log("B");
+}
+else if (score <= 100) {
+    console.log("A")
+}
